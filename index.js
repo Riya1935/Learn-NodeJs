46539234f92059df4f576e8ce0eca93c76584c82
riya1935 <riyathakur1935@gmail.com>
2020-09-02 20:49:31 +0530

    Simple node examples

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function SolveRect(l,b)  {
     console.log("solving for rectangle with l = " + l + " and width = " + b);
